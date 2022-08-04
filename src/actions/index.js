b5faf73ceaae3a0132125f7f78c6d263febf506a
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2022-08-04 17:43:07 +0600

    Created createAction functin on action creaters file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import {createAction} from '@reduxjs/toolkit'
+
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
     request("http://localhost:3001/heroes")
@@ -12,70 +14,29 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+export const heroesFetching = createAction('HEROES_FETCHING')
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
 
-export const filtersFetching = () => {
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
+export const heroesFetched = createAction('HEROES_FETCHED')
 
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
 
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
+export const heroesFetchingError  = createAction('HEROES_FETCHING_ERROR')
 
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
 
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
+export const filtersFetching = createAction('FILTERS_FETCHING')
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
 
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
+export const filtersFetched = createAction('FILTERS_FETCHED')
+
+
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR')
+
+
+export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED')
+
+
+export const heroCreated = createAction('HERO_CREATED')
+
+
+export const heroDeleted = createAction('HERO_DELETED')
